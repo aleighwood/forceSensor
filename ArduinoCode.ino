66a89727d011dd36413945e183e8f45ab2a91870
Arthur Leigh-Wood <86426050+aleighwood@users.noreply.github.com>
2024-01-21 15:01:57 +0000

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,20 +1,21 @@
-int sensorPin = A0;  // select the input pin for the potentiometer
+// initalise variables
+int sensorPin = A0;  
 int sensorValue;
 char userInput;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // for communication with computer
 }
 
 void loop() {
   if (Serial.available() > 0) {
 
-    userInput = Serial.read();
+    userInput = Serial.read(); // read serial input from computer
       
     if(userInput == 'g'){
-      sensorValue = analogRead(sensorPin);
-      Serial.println(sensorValue);
+      sensorValue = analogRead(sensorPin); // read sensor 
+      Serial.println(sensorValue); // send sensor value to computer
     }
   }
 }
