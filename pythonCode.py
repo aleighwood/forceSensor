b83e61b3cd8888c2e8e11dc7414ed6354875b208
Arthur Leigh-Wood <86426050+aleighwood@users.noreply.github.com>
2024-01-21 15:08:22 +0000

    Update pythonCode.py

--- pythonCode.py
+++ pythonCode.py
@@ -1,23 +1,25 @@
+# import libaries
 import pygame
 from sys import exit
 import serial  
 import time
 
+# set up serial communication
 ser = serial.Serial("COM4", baudrate=9600, timeout=1)
-print(ser) # my Ardunio is connecte to serial port COM4, this will likely be different on your computer
+print(ser) # my Ardunio is connected to serial port COM4, this will likely be different on your computer
 time.sleep(3) # sleep to allow connection 
 
 
 def getValues():  # function for getting value from serial port
     ser.write(b"g")  # sends 'g' to arduino via serial
     ardunioData = ser.readline().decode("ascii")  # reads serial port
-    # print("here: ")
-    # print(ardunioData)
-    # print(type(ardunioData))
+    
+    # print(ardunioData) # for debugging
+    # print(type(ardunioData)) # for debugging
     return int(ardunioData)
 
 
-def translate(value, leftMin, leftMax, rightMin, rightMax):
+def translate(value, leftMin, leftMax, rightMin, rightMax): # map function from 0 1023 to 0 255 
     # Figure out how 'wide' each range is
     leftSpan = leftMax - leftMin
     rightSpan = rightMax - rightMin
@@ -37,10 +39,10 @@ def getColour(value): #create rgb value from force value
     return colours
 
 
-pygame.init()
-width, height = 800, 800
+pygame.init() #initialise pygame
+width, height = 800, 800 # set window dimensions
 screen = pygame.display.set_mode((width, height))  # create screen with dimensions
-pygame.display.set_caption("Runner")  # set window title
+pygame.display.set_caption("Rectangle")  # set window title
 clock = pygame.time.Clock()
 rgb = [255, 0, 0]
 
@@ -48,15 +50,14 @@ test_surface = pygame.Surface((800, 800))  # create surface
 test_surface.fill(rgb, rect=None, special_flags=0)
 
 while True:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # for quitting the programme
         if event.type == pygame.QUIT:  # exit game
             pygame.quit()
             ser.close() # close com port
             exit()  # quits all code
 
     rgb = getColour(getValues())  # get rgb value, using serial value
-    print(rgb)
-
+   
     test_surface.fill(rgb, rect=None, special_flags=0) # fill surface
 
     screen.blit(test_surface, (0, 0))  # display surface
